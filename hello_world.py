464140d69905f2b4cd0c719450f6e38c90ab5ba3
婉晴 <wzx384748@digital-engine.com>
2023-03-13 09:17:10 +0800

    feat: 认识 python 变量

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+mes = 'hey you'
+mes = 'hey me'
+print(mes)
+
+mes = 'hey boy'
+print(mes)
+heyYou = 1
+print(heyYou)
+
+mess = "hey you 'a boy' lalala"
+print(mess) # 123
+print(1_00) # 100
+print(10_00) # 1000
